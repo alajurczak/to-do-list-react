77ef104b230087d029cb637432dcacac7ade8e61
Alicja Jurczak <ala.jurczak@gmail.com>
2022-12-11 12:33:25 +0100

    code formatting

--- src/Form/index.js
+++ src/Form/index.js
@@ -7,7 +7,7 @@ const Form = ({ addNewTask }) => {
     const onFormSubmit = (event) => {
         event.preventDefault();
         const trimedNewTaskContent = newTaskContent.trim();
-        if(!trimedNewTaskContent) {
+        if (!trimedNewTaskContent) {
             return;
         }
 
@@ -17,7 +17,12 @@ const Form = ({ addNewTask }) => {
 
     return (
         <form className="form" onSubmit={onFormSubmit}>
-            <input value={newTaskContent} className="form__newTask" placeholder="Co jest do zrobienia?" autoFocus onChange={({ target }) => setNewTaskContent(target.value)} />
+            <input
+                value={newTaskContent}
+                className="form__newTask"
+                placeholder="Co jest do zrobienia?"
+                autoFocus
+                onChange={({ target }) => setNewTaskContent(target.value)} />
             <button className="form__button">Dodaj zadanie</button>
         </form>
     )
--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,13 +1,21 @@
 import "./style.css";
 
-const Tasks = ({tasks, hiddenDone, removeTasks, toggleTaskDone}) => (
+const Tasks = ({ tasks, hiddenDone, removeTasks, toggleTaskDone }) => (
     <ul className="tasks">
         {tasks.map(task => (
             <li key={task.id}
                 className={`list ${task.done && hiddenDone ? "list__item--hidden" : ""}`}>
-                <button className="list__button list__button--done" onClick={() => toggleTaskDone(task.id)}>{task.done ? "✓" : ""}</button>
+                <button
+                    className="list__button list__button--done"
+                    onClick={() => toggleTaskDone(task.id)}>
+                    {task.done ? "✓" : ""}
+                </button>
                 <p className={`${task.done ? " list__item--done" : "list__item"}`}>{task.content}</p>
-                <button className="list__button list__button--remove" onClick={() => removeTasks(task.id)}>🗑</button>
+                <button
+                    className="list__button list__button--remove"
+                    onClick={() => removeTasks(task.id)}>
+                    🗑
+                </button>
             </li>
         ))}
     </ul >
