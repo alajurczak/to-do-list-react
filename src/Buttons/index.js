ad6c062e77e99f907c7817152dd40be39cbb6f1b
Alicja Jurczak <ala.jurczak@gmail.com>
2022-12-13 17:19:36 +0100

    shortened version of React.Fragment

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -4,12 +4,12 @@ import "./style.css";
 const Buttons = ({ tasks, hiddenDone, toggleHideDone, setAllDone }) => (
     <div className="buttons">
         {tasks.length > 0 && (
-            <React.Fragment>
+            <>
                 <button onClick={toggleHideDone} className="button">{hiddenDone ? "Pokaż" : "Ukryj"} ukończone</button>
                 <button onClick={setAllDone} className="button" disabled={tasks.every(({ done }) => done)}>
                     Ukończ wszystkie
                 </button>
-            </React.Fragment>
+            </>
         )}
     </div>
 
