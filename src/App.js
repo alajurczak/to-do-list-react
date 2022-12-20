3369ffb5d4a3b4228634d63ff734ef27c046e275
Alicja Jurczak <ala.jurczak@gmail.com>
2022-12-20 17:11:12 +0100

    simpler notation of localStorage

--- src/App.js
+++ src/App.js
@@ -10,11 +10,8 @@ import Container from "./Container";
 function App() {
   const [hiddenDone, setHiddenDone] = useState(false);
 
-  const tasksFromLocalStorage = localStorage.getItem("tasks");
-
-  const [tasks, setTasks] = useState(
-    tasksFromLocalStorage ? JSON.parse(tasksFromLocalStorage) : []);
-
+  const [tasks, setTasks] = useState(JSON.parse(localStorage.getItem("tasks")) || []);
+  
   useEffect(() => {
     localStorage.setItem("tasks", JSON.stringify(tasks));
   }, [tasks]);
