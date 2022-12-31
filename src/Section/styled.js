b6569be2f605991bb99b1f5e0ecbfdce0e3a52aa
Alicja Jurczak <ala.jurczak@gmail.com>
2022-12-31 20:08:41 +0100

    theme colors added at Section

--- src/Section/styled.js
+++ src/Section/styled.js
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
 export const StyledSection = styled.section`
-    background-color: rgb(255, 255, 255);
-    border: 2px solid rgba(232, 232, 232);
+    background-color: ${({theme}) => theme.color.white};
+    border: 2px solid ${({theme}) => theme.color.mercury};
     margin-bottom: 5px;
 
     @media (max-width: 767px) {
@@ -13,7 +13,7 @@ export const StyledSection = styled.section`
 export const Header = styled.div`
     display: grid;
     grid-template-columns: 1fr auto;
-    border-bottom: 2px solid rgba(232, 232, 232);
+    border-bottom: 2px solid ${({theme}) => theme.color.mercury};
 
     @media (max-width: 767px) {
         grid-template-columns: 1fr;
@@ -22,7 +22,7 @@ export const Header = styled.div`
 
 export const Title = styled.h2`
     padding: 20px;
-    background-color: rgba(255, 255, 255);
+    background-color: ${({theme}) => theme.color.white};
     margin: 0;
     font-size: 20px;
 `;
