9a90438f96f88e227027ecbf9d8aad0d8f31de3a
Alicja Jurczak <ala.jurczak@gmail.com>
2022-12-31 19:44:10 +0100

    theme added

---
+++ src/theme.js
@@ -0,0 +1,19 @@
+export const theme = {
+    color: {
+        teal: "#008080",
+        java: "#16baba",
+        turquoise: "#2fe5e5",
+        silver: "cccccc",
+        white: "ffffff",
+        blueChill: "#0ca0a0",
+        mercury: "#e8e8e8",
+        bilbao: "#148414",
+        forestGreen: "#23b023",
+        cinnabar: "#e93838",
+        carnation: "#f26565",
+    },
+    
+    breakpoint: {
+        mobileMax: 767,
+    },
+}
