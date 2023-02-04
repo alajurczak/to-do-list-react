7c7a10ba2c6f668cb1ae07b2e439f72101a0eace
Alicja Jurczak <ala.jurczak@gmail.com>
2023-02-04 18:58:27 +0100

    setting disabled on button with loading

--- src/features/tasks/TasksPage/ExampleTasksButton/index.js
+++ src/features/tasks/TasksPage/ExampleTasksButton/index.js
@@ -1,16 +1,17 @@
-import { useDispatch } from "react-redux";
-import { fetchExampleTasks } from "../../tasksSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchExampleTasks, selectLoading } from "../../tasksSlice";
 import { StyledButtons } from "../Buttons/styled";
 import { Button } from "../Buttons/styled";
 
 
 const ExampleTasksButton = () => {
     const dispatch = useDispatch();
-    
+    const loading = useSelector(selectLoading);
+
     return (
         <StyledButtons>
-            <Button onClick={() => dispatch(fetchExampleTasks())}>
-                Pobierz przykładowe zadania
+            <Button onClick={() => dispatch(fetchExampleTasks())} disabled={loading}>
+                {loading ? "Ładowanie..." : "Pobierz przykładowe zadania"}
             </Button>
         </StyledButtons>
     )
